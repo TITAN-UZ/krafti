c344837f7ff2e11d7c39825329115d8beab71946
Agel_Nash <agel-nash@yandex.com>
2020-05-18 08:35:42 +0000

    feat(tests): Добавить тесты для favorites и like контроллеров

---
+++ core/tests/Feature/Controllers/User/FavoritesTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests\Feature\Controllers\User;
+
+use App\Controllers\User\Favorites as Controller;
+use App\Tests\Feature\Controllers\RequestStatusTrait;
+use App\Tests\TestCase;
+
+class FavoritesTest extends TestCase
+{
+    use RequestStatusTrait;
+    protected $user = true;
+
+    protected function getUri()
+    {
+        return '/api/user/favorites';
+    }
+
+    protected function getController()
+    {
+        return Controller::class;
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->app->any($this->getUri(), [$this->getController(), 'process']);
+    }
+
+    public function testPutSuccess()
+    {
+        $this->putSuccess(['course_id' => 1]);
+    }
+
+    public function testGetSuccess()
+    {
+        $this->getSuccess([]);
+    }
+
+    public function testDeleteSuccess()
+    {
+        $this->requestSuccess('DELETE', ['course_id' => 1]);
+    }
+}
---
+++ core/tests/Feature/Controllers/User/LikeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests\Feature\Controllers\User;
+
+use App\Controllers\User\Like as Controller;
+use App\Model\Lesson;
+use App\Tests\Feature\Controllers\RequestStatusTrait;
+use App\Tests\TestCase;
+
+class LikeTest extends TestCase
+{
+    use RequestStatusTrait;
+    protected $user = true;
+
+    protected function getUri()
+    {
+        return '/api/user/like';
+    }
+
+    protected function getController()
+    {
+        return Controller::class;
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->app->any($this->getUri(), [$this->getController(), 'process']);
+    }
+
+    public function testPostSuccess()
+    {
+        $lesson = Lesson::firstOrFail();
+
+        $this->postSuccess(['lesson_id' => $lesson->getKey()]);
+    }
+
+    public function testGetSuccess()
+    {
+        $this->getSuccess([]);
+    }
+}
