0f27c7b61ce0e60fb2c038d0ff427ded6ea78f26
Vasily Naumkin <bezumkin@yandex.ru>
2020-07-28 22:07:27 +0700

    fix(frontend): исправление работы шапки админки в safari

--- frontend/src/admin/components/app-header.vue
+++ frontend/src/admin/components/app-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar toggleable="md" class="d-block d-md-flex border-top-0 border-left-0 border-right-0 border-bottom">
+  <b-navbar toggleable="md" class="d-block border-top-0 border-left-0 border-right-0 border-bottom">
     <div class="container">
       <b-navbar-brand href="/">
         <img src="~assets/images/logo.svg" alt="Krafti.ru" />
@@ -9,14 +9,14 @@
         <fa :icon="['fas', 'bars']" />
       </b-button>
 
-      <b-collapse id="nav-collapse" is-nav>
+      <b-collapse id="nav-collapse" is-nav style="height: 50px;">
         <b-navbar-nav class="flex-md-grow-1 flex-wrap w-100">
-          <b-nav-item v-for="(item, idx) in mainMenu" :key="idx" :to="item.to">
+          <b-nav-item v-for="(item, idx) in mainMenu" :key="idx" :to="item.to" class="text-right text-md-left">
             {{ item.title }}
           </b-nav-item>
         </b-navbar-nav>
         <div class="d-flex ml-auto">
-          <div class="d-flex flex-column">
+          <div class="d-flex flex-column ml-auto">
             <div class="text-right text-nowrap">{{ $auth.user.fullname }}</div>
             <a href="#" class="small text-right mt-1" @click.prevent="onLogout">Выход</a>
           </div>
